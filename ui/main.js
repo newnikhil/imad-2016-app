6dca8f054d126530985729ac832e5ec7bbedeeb6
Nikhil <newnikhilnew@gmail.com>
2016-10-16 19:49:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,35 +1,26 @@
 //Counter code
 var button = document.getElementById("counter");
-var counter = 0;
+
 button.onclick = function()
 {
-  //Make a request to the counter endpoint
-  
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
   //Capture the response and store it in a variable
-  
-  //Render the variable in the correct span
-  counter++;
-  var span = document.getElementById("count");
-  span.innerHTML = counter.toString();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        //Not done yet
+    };
+    
+    //Make the request
+    request.open('GET', "http://newnikhil.imad.hasura-app.io/counter", true);
+    request.send(null);
 };
-/*console.log('Loaded!');
-
-//Change the content of main-text element
-
-var element = document.getElementById("main-text");
-element.innerHTML = "New Text Value";
-
-//move the image
-var img = document.getElementById("madi");
-var marginLeft = 0;
-
-function moveRight() {
-  marginLeft += 1;
-  img.style.marginLeft = marginLeft + "px";
-}
-
-img.onclick = function() {
-  var interval = setInterval(moveRight, 50);
-  console.log(marginLeft);
-};
-*/
