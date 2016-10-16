3a3dc219642a560b1cd39a4c2c5247f4a33b479b
Nikhil <newnikhilnew@gmail.com>
2016-10-16 23:01:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ app.get('/counter', function(req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) {// URL: /submit-name?name=
     //Set the name from request
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     
     res.send(JSON.stringify(names));
