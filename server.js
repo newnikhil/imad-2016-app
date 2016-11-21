464c41563c1e20165c76f9ff1471373085965405
Nikhil <newnikhilnew@gmail.com>
2016-11-21 17:42:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,6 +113,31 @@ app.post('/create-user', function(req, res) {
     });
 });
 
+app.post('/login', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result) {
+       if (err) {
+            res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+               res.send('username/password is invalid');
+           } else {
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt);
+               
+               if (hashedPassword === dbString) {
+                   res.send('credentials correct!');
+               } else {
+                   res.send('forbidden');
+               }
+           }
+       }
+    });
+});
+
 var counter = 0;
 app.get('/counter', function(req, res) {
    counter++;
