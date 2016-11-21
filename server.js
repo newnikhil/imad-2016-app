a43f31546832a3d320f56f81c7bae3ce8607c5d8
Nikhil <newnikhilnew@gmail.com>
2016-11-21 20:41:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.post('/login', function(req, res) {
                var hashedPassword = hash(password, salt);
                
                if (hashedPassword === dbString) {
-//                   req.session.auth = {userId: result.rows[0].id};
+                   req.session.auth = {userId: result.rows[0].id};
                    
                    res.send('credentials correct!');
                } else {
