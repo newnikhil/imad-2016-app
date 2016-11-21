1bf9d9336c8e1d2ac23e074610d3053695c42eaf
Nikhil <newnikhilnew@gmail.com>
2016-11-21 17:18:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser, json());
+app.use(bodyParser.json());
 
 var articleOne = {
     title: 'Article One | Nikhil',
