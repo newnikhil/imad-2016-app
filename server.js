3e8fee3a39f70acb5bb85e22dbb57412c057a611
Nikhil <newnikhilnew@gmail.com>
2016-11-21 13:27:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,8 +90,8 @@ function hash(input, salt) {
 }
 
 app.get('/hash/:input', function(req, res) {
-	var hashString = hash(req.params.input, 'this-is-some-random-string');
-	res.send(hashString);
+	var hashedString = hash(req.params.input, 'this-is-some-random-string');
+	res.send(hashedString);
 });
 
 var counter = 0;
