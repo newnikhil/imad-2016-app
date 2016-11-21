f2f43392d232f8557d29671a7002b1be20f4f5c3
Nikhil <newnikhilnew@gmail.com>
2016-11-22 01:34:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,36 @@
+var art = document.getElementById('article2');
+
+art.onclick = function()
+{
+       //Create a request object
+    var request = new XMLHttpRequest();
+    
+  //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status == 200) {
+                //alert("User successfully logged in");
+                var req = new XMLHttpRequest();
+                            
+                req.open('POST', "http://newnikhil.imad.hasura-app.io/article-two", true);
+                req.send(null);
+            } else if (request.status === 403) {
+                alert("You need to login first!");
+            } else if (request.status === 500) {
+                alert("Something went wrong on the server");
+            }
+        }
+        //Not done yet
+    };
+    
+    //Make the request
+    request.open('POST', "http://newnikhil.imad.hasura-app.io/check-login", true);
+    request.send(null);
+    //Make a request to the server and send the name
+    //Capture a list of names and render as a list
+
+};
 /*
 //Counter code
 var button = document.getElementById("counter");
