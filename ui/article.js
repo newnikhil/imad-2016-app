9283a8c4d23a7d5dbb0658ba934e38d7002390a5
Nikhil <newnikhilnew@gmail.com>
2016-11-22 15:35:33 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,1 +1,13 @@
+var post = document.getElementById('post');
 
+post.onclick = function() {
+  var li = document.createElement('li');
+  var text = document.getElementById('comment').value;
+  
+  if (text.length > 0) {
+    li.textContent = text;
+  
+    var ul = document.getElementById('postList');
+    ul.appendChild(li);
+  }
+};
