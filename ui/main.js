583b959f1fd8b9b1a940f20136f585ce2d6d9ac8
Nikhil <newnikhilnew@gmail.com>
2016-11-22 15:13:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,13 @@ var post = document.getElementById('post');
 post.onclick = function() {
   var li = document.createElement('li');
   var text = document.getElementById('comment').value;
-  li.textContent = text;
   
-  var ul = document.getElementById('postList');
-  ul.appendChild(li);
+  if (text.length > 0) {
+    li.textContent = text;
+  
+    var ul = document.getElementById('postList');
+    ul.appendChild(li);
+  }
 };
 
 var art = document.getElementById('article2');
