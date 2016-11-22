4c4b0bd9e398275df9ffcb5db8f7465f90f9bb25
Nikhil <newnikhilnew@gmail.com>
2016-11-22 14:05:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ var post = document.getElementById('post');
 post.onclick = function() {
   var li = document.createElement('li');
   var text = document.getElementById('comment').textContent;
+  alert(text);
   li.textContent = text;
   
   var ul = document.getElementById('postList');
