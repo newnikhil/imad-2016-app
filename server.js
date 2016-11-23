8b2777f77cd58a0464ca6211318f08d4bfb630cc
Nikhil <newnikhilnew@gmail.com>
2016-11-24 00:16:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,10 +68,10 @@ function createTemplate (data) {
                 <hr />
                 <h4>Comments</h4>
                 <div id="comment_form">
-                      <textarea id='comment' style='display: none' class="form-control status-box" rows="2" placeholder="What's on your mind?"></textarea>
+                      <textarea id='comment' style='display: none, width: 100%' class="form-control status-box" rows="2" placeholder="What's on your mind?"></textarea>
                 </div>
                 <div class="button-group pull-right">
-                    <a href="#" id="post" style="display: none, width: 100%" class="btn btn-primary">Post</a>
+                    <a href="#" id="post" style="display: none" class="btn btn-primary">Post</a>
                 </div>
                 <div id="comments">
                     <center>Loading comments...</center>
