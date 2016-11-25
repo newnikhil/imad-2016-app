2dd27c37f86bcf3153315177f1760b1c46d48d25
Nikhil <newnikhilnew@gmail.com>
2016-11-26 02:25:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -276,7 +276,7 @@ app.get('/load-articles', function(req, res) {
 });
 
 app.get('/ui/loginPopup', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'loginPopup.html'))
+    res.sendFile(path.join(__dirname, 'loginPopup.html'))
 });
 
 app.get('/ui/style.css', function (req, res) {
